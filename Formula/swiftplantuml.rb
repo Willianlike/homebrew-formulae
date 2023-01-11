88634e40991a6e1ec094c4982cfc0c1d9ea06a7b
Vilian Iaumbayev <vilian.iaumbayev@indriver.com>
2023-01-12 00:27:19 +0530

    fix formulae version

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -1,7 +1,7 @@
 class Swiftplantuml < Formula
   desc "Generate UML class diagrams from Swift sources"
   homepage "https://github.com/WillianLike/SwiftPlantUML"
-  url "https://github.com/WillianLike/SwiftPlantUML/archive/0.7.0.tar.gz"
+  url "https://github.com/WillianLike/SwiftPlantUML/archive/0.8.1.tar.gz"
   sha256 "b3c86fb68d6cbec19aab8b1c5cb3431f13b584dbe35ce82ea8d5b6143e16b0ea"
   license "MIT"
 
